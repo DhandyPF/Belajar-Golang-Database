6c4555073e71df21f9dd2f0eaf10c25acfeb73ab
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-06-29 11:41:59 +0700

    Membuat koneksi ke Database belajar_golang_database

--- database_test.go
+++ database_test.go
@@ -11,6 +11,10 @@ func TestEmpty(t *testing.T) {
 
 }
 
-func TesOpenConnection(t *testing.T) {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)")
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306/belajar_golang_database)")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 }
