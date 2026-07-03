cdf243dd9508ed0069d6eb17730da43d4c3c26a5
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-07-03 23:42:27 +0700

    Belajar Mengkonversi Tipe Data dari MySQL ke Golang Database

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecutionSQL(t *testing.T) {
@@ -12,7 +13,7 @@ func TestExecutionSQL(t *testing.T) {
 
 	ctx := context.Background()
 	
-	script := "INSERT INTO customer(id, name, email, balance, rating, birth_date, married, created_at) VALUES('1', 'Dhandy', 'dhandy@gmail.com', 1000, 5.0, '2007-03-10', FALSE, CURRENT_TIMESTAMP)"
+	script := "INSERT INTO customer(id, name, email, balance, rating, birth_date, married) VALUES('1', 'Dhandy', 'dhandy@gmail.com', 1000, 5.0, '2007-03-10', FALSE)"
 	_, err := db.ExecContext(ctx, script)
 	if err != nil {
 		panic(err)
@@ -27,7 +28,7 @@ func TestQuerySQL(t *testing.T) {
 
 	ctx := context.Background()
 	
-	script := "SELECT * FROM customer"
+	script := "SELECT id, name FROM customer"
 	rows, err := db.QueryContext(ctx, script)
 	if err != nil {
 		panic(err)
@@ -44,6 +45,46 @@ func TestQuerySQL(t *testing.T) {
 		}
 		fmt.Println("ID : ", id)
 		fmt.Println("Name : ", name)
+	}
+
+	defer rows.Close()
+}
+
+func TestQuerySQLComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name, email string
+		var balance int32
+		var rating float64
+		var birth_date, created_at time.Time
+		var married bool
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birth_date, &married, &created_at)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("===========================================")
+		fmt.Println("ID : ", id)
+		fmt.Println("Name : ", name)
+		fmt.Println("Email : ", email)
+		fmt.Println("Balance : ", balance)
+		fmt.Println("Rating : ", rating)
+		fmt.Println("Birth Date : ", birth_date)
+		fmt.Println("Married : ", married)
+		fmt.Println("Created At : ", created_at)
+		fmt.Println("===========================================")
 	}
 
 	defer rows.Close()
