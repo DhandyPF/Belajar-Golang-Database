fc4f05a87c9fa75178cd8b696df4865cd97f0dda
Dhandy Putra Fahruddin <dhandy.pf@gmail.com>
2026-07-03 23:02:40 +0700

    Mencoba Query SQL di Golang

--- sql_test.go
+++ sql_test.go
@@ -20,3 +20,31 @@ func TestExecutionSQL(t *testing.T) {
 
 	fmt.Println("Success insert new Customer")
 }
+
+func TestQuerySQL(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	
+	script := "SELECT * FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var id string
+		var name string
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("ID : ", id)
+		fmt.Println("Name : ", name)
+	}
+
+	defer rows.Close()
+}
